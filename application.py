43725fc0c4837e794673d1f16888b898f05fff7d
Pooja <bhatipooja94@gmail.com>
2021-09-18 14:31:22 +0530

    'aws'

--- application.py
+++ application.py
@@ -42,8 +42,9 @@ def index():
         return render_template('index.html')
 
 
-port = int(os.getenv("PORT"))
+#port = int(os.getenv("PORT"))
 
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=8001, debug=True)
-    app.run(host='0.0.0.0', port=port) # running the app
+    #app.run(host='0.0.0.0', port=port) # running the app
+    app.run(debug=True)
