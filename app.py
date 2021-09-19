784c4e4fe08c60ca599045a05b440a248bbd42d4
Pooja <bhatipooja94@gmail.com>
2021-09-19 10:36:42 +0530

    update

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ port = int(os.getenv("PORT"))
 
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=8001, debug=True)
-    app.run(host='0.0.0.0', port=port) # running the app
-    # app.run(debug=True)
+    # app.run(debug=True) # running the app
+    app.run(host='0.0.0.0', port=port)
